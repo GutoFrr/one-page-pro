7f08987a9c43b962875179deae8337104d359ae9
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-05-09 11:59:37 -0300

    theme-colors-added

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>One Page Pro</title>
+      </Head>
+
+      <h1>NextJS Structure</h1>
+      <p>Next.js development structure by JVM's employee Gustavo Ferreira.</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+const theme = {
+  colors: {
+    text: '#ffffff',
+    primary: '#b1d234',
+    header: '#1b1b1b',
+    headerText: '#999999',
+    menu: '#161616',
+    menuItem: '#a0a0a0',
+    menuItemSelected: '#ffffff',
+    facebook: '#4267B2',
+    googlePlus: '#DB4437',
+    linkedIn: '#0e76a8',
+    twitter: '#1DA1F2',
+    services: '#ffffff',
+    servicesTitle: '#1e1e1e',
+    servicesText: '#515151',
+    selectedServicesTitle: '#ffffff',
+    selectedServicesText: '#cecece',
+    works: '#f5f5f5',
+    worksTitle: '#353535',
+    worksSubTitle: '#757575',
+    worksText: '#5e5e5e',
+    about: '#ffffff',
+    aboutText: '#6d6d6d',
+    aboutList: '#3d3d3d',
+    newsText: '#efefef',
+    testimonialText: '#f9f9f9',
+    testimonialAuthor: '#ffffff',
+    testimonialAuthorRole: '#d3d3d3'
+  }
+}
+
+export default theme
